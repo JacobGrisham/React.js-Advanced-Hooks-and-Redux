40b3bae3613acee57b8bd71e22eea8addcfe1740
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-04-16 23:26:42 -0700

    :recycle: Refactor App with hooks
    employ useState and useEffect for robots and searchfield

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Scroll from './Scroll'
 import CardList from './CardList';
 import SearchBox from './SearchBox'
@@ -7,49 +7,40 @@ import '../assets/App.css';
 
 // the virtual DOM is just a javascript object that collects the state
 // React then passes the state to child components as props
-class App extends Component {
-  // state represents properties that can change. They usually live in the parent component
-  constructor() {
-    // super() calls the constructor of component
-    super();
-    // the keyword this refers to class App
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+function App() {
 
-  componentDidMount() {
+  // Create states using React Hooks
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => this.setState({robots: users}));
+    .then(users => setRobots(users));
+    // The second argument tells useEffect what to listen for to trigger an update
+    // An empty array is a shortcut for componentDidMount
+  }, [])
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   }
 
-  onSearchChange = (event) => {
-    this.setState({
-      searchfield: event.target.value
-    });
-  }
-
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-    
-    return !robots.length ? <h1>Loading...</h1> : (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        {/* Since App is an object, we need to reference functions and properties as the keyword this */}
-        <SearchBox searchChange={ this.onSearchChange }/>
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={ filteredRobots } />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
-  }
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
+  
+  return !robots.length ? <h1>Loading...</h1> : (
+    <div className='tc'>
+      <h1 className='f1'>RoboFriends</h1>
+      {/* Since App is an object, we need to reference functions and properties as the keyword this */}
+      <SearchBox searchChange={ onSearchChange }/>
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={ filteredRobots }/>
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded';
+import FavoriteRoundedIcon from '@material-ui/icons/FavoriteRounded';
 
 // Destructuring the properties and placing it in the parameters of the function for cleaner code
 // These properties, or props, were passed down from the parent component
 function Card({ id, name, email }) {
 
+  const [favorite, setFavorite] = useState(false);
+
+  const onClickFavorite = () => {
+    setFavorite(true)
+  }
+
+  useEffect(() => {
+    console.log(favorite);
+    // The second argument tells useEffect what to listen for to trigger an update
+    // Only run when favorite changes
+  }, [favorite])
+
   return(
     <div className='bg-gray dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
+      <button onClick={ onClickFavorite } style={{ marginLeft: 'auto',  border: 'none', outline: 'none', backgroundColor: 'transparent'}}>
+        { favorite ? <FavoriteRoundedIcon style={{ color: 'red' }} /> : <FavoriteBorderRoundedIcon/>}
+      </button>
       <img alt='robot' src={`https://robohash.org/${ id }?200x200`}></img>
-      <div>
+      <div style={{ color: 'white' }}>
         <h2>{ name }</h2>
         <p>{ email }</p>
       </div>
