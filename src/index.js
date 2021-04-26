7cc847175ed05ccb77616b8b906233f4116d486a
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-04-26 16:18:15 -0700

    :sparkles: Add Progressive Web App Functionality
    Add code and files from 'create-react-app my-app --template cra-template-pwa' to enable progressive web app functionality. Add service-worker.js and serviceWorkerRegistration.js files. Register service worker in index.js. Add maskable icon in manifest.json. Add workbox dependencies in package.json.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // React
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 // Redux
 import { Provider } from 'react-redux';
@@ -33,3 +34,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
